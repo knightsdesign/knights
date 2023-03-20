0a8968327a0ffc2d15451af62ee769f13bcb9c68
Kevin Nguyen <me@kndwin.dev>
2023-03-20 15:43:17 +1100

    fix: html formatting

--- src/features/home/components/contact-form.tsx
+++ src/features/home/components/contact-form.tsx
@@ -2,20 +2,14 @@
 
 import { linkButton } from "../components";
 import Image from "next/image";
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-  DialogTitle,
-  DialogHeader,
-} from "~/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger, DialogTitle } from "~/ui/dialog";
 
 const DialogContactForm = () => {
   return (
     <Dialog>
       <DialogTrigger className={linkButton()}>Contact us</DialogTrigger>
       <DialogContent className="lg:max-w-7xl">
-        <DialogTitle>Let's get started!</DialogTitle>
+        <DialogTitle>{`Let's get started!`}</DialogTitle>
         <div className="relative flex max-h-full justify-center overflow-hidden md:px-12 lg:px-0">
           <div className="relative z-10 flex flex-1 flex-col bg-white px-4 py-10 shadow-2xl sm:justify-center md:flex-none md:px-28 lg:py-24">
             <div className="mx-auto w-full max-w-md sm:px-4 md:w-96 md:max-w-sm md:px-0">
