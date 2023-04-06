b649a79fe5899565eaf3980f7177f0462f5f51d6
Amr Hamdy <amr@knightsdesign.studio>
2023-04-06 21:12:35 +1000

    added horse model logo

---
+++ src/app/components/3D-logo.tsx
@@ -0,0 +1,106 @@
+import * as THREE from "three";
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Hair: THREE.Mesh;
+    Tail: THREE.Mesh;
+    Base: THREE.Mesh;
+    body: THREE.Mesh;
+    Ears: THREE.Mesh;
+    Eyes: THREE.Mesh;
+  };
+  materials: {};
+};
+
+export function Model(props: JSX.IntrinsicElements["group"]) {
+  const group = useRef<THREE.Group>(null);
+  const { nodes, materials } = useGLTF(
+    "/fullbody-logo-rigged-posed.glb"
+  ) as GLTFResult;
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <mesh
+          name="Hair"
+          castShadow
+          receiveShadow
+          geometry={nodes.Hair.geometry}
+        >
+          <meshStandardMaterial
+            color={0xffffff}
+            roughness={0}
+            metalness={0.2}
+          />
+        </mesh>
+        <mesh
+          name="Tail"
+          castShadow
+          receiveShadow
+          geometry={nodes.Tail.geometry}
+        >
+          <meshStandardMaterial
+            color={0xffffff}
+            roughness={0}
+            metalness={0.2}
+          />
+        </mesh>
+        <mesh
+          name="Base"
+          castShadow
+          receiveShadow
+          geometry={nodes.Base.geometry}
+        >
+          <meshStandardMaterial
+            color={0xffffff}
+            roughness={0}
+            metalness={0.2}
+          />
+        </mesh>
+        <mesh
+          name="body"
+          castShadow
+          receiveShadow
+          geometry={nodes.body.geometry}
+          position={[0, 0.18, 0.25]}
+        >
+          <meshStandardMaterial
+            color={0xffffff}
+            roughness={0}
+            metalness={0.2}
+          />
+        </mesh>
+        <mesh
+          name="Ears"
+          castShadow
+          receiveShadow
+          geometry={nodes.Ears.geometry}
+          position={[0, 0.18, 0.25]}
+        >
+          <meshStandardMaterial
+            color={0xffffff}
+            roughness={0}
+            metalness={0.2}
+          />
+        </mesh>
+        <mesh
+          name="Eyes"
+          castShadow
+          receiveShadow
+          geometry={nodes.Eyes.geometry}
+          position={[0, 0.18, 0.25]}
+        >
+          <meshStandardMaterial
+            color={0xffffff}
+            roughness={0}
+            metalness={0.2}
+          />
+        </mesh>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/fullbody-logo-rigged-posed.glb");
--- src/app/components/box.tsx
+++ src/app/components/box.tsx
@@ -3,16 +3,16 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { Model } from "./3D-logo";
 
 export const BlackBox = () => (
   <Suspense fallback={null}>
-    <Canvas className="mx-auto w-full bg-black object-cover object-center lg:ml-auto">
-      <ambientLight intensity={0.3} />
-      <directionalLight color="white" position={[0, 2, 5]} />
-      <mesh rotation={[Math.PI, 0, 0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={"white"} />
-      </mesh>
+    <Canvas className="mx-auto w-full object-cover object-center lg:ml-auto">
+      <ambientLight intensity={0.2} />
+      <directionalLight color="white" position={[0, 2, 5]} intensity={0.3} />
+      <directionalLight color="white" position={[0, 2, -5]} intensity={0.3} />
+
+      <Model position={[0, -1.3, 0]} scale={1.2} />
       <OrbitControls autoRotate enableZoom={false} />
     </Canvas>
   </Suspense>
